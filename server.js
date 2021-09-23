d1d4ed68250ea0ce7baf1c4dba27986af26c4ecf
Bashar-Owainat <owainatbashar13@gmail.com>
2021-09-23 05:49:11 +0300

    solved

--- server.js
+++ server.js
@@ -6,42 +6,47 @@ const cors = require('cors');
 
 const server = express();
 const axios = require('axios');
-const weatherData = require('./data/weather.json');
+// const weatherData = require('./data/weather.json');
 
 const PORT = process.env.PORT;
 server.use(cors());
 
-class weatherData{
-    constructor(date, description){
-        this.date = date;
-        this.description = description;
+class WeatherData{
+    constructor(element){
+        this.date = element.datetime;
+        this.description = element.weather.description;
     }
 }
 server.get('/weather', getWeatherHandler);
 server.get('/test', testHandler);
-server.get('*', notFoundHandler)
+server.get('*', notFoundHandler);
 
  function getWeatherHandler(req, res){
     
     let cityname =  req.query.cityname;
-   //https://api.weatherbit.io/v2.0/current?key=&city=
+   //https://api.weatherbit.io/v2.0/forecast/daily?key=&city=&days=3
    
     
-    let url = `https://api.weatherbit.io/v2.0/current?key=${process.env.WEATHER_API_KEY}&city=${cityname}`
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&city=${cityname}&days=3`
         console.log(url);
 
         axios.get(url).then(item => {
-            let newArr = item.data.map(e =>{
-                return new weatherData()
+            console.log(item);
+            let newArr = item.data.data.map(element =>{
+                return new WeatherData(element);
             })
-        
+            res.send(newArr);
+        }).catch(error =>{
+            res.send(error)
         })
 
+        
+       
      
 }
 
 function testHandler(req,res){
-    res.send(weatherData);
+    res.send('test');
 }
 function notFoundHandler(req,res){
     res.status(404).send('route is not found')
